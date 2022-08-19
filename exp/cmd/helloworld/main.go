8897e1058ce78d9b34586d34c243f903e139f3b9
Ronoaldo JLP <ronoaldo@gmail.com>
2022-08-19 14:27:43 -0300

    refactor: created some gl/glfw abstractions.
    Initial version of Window, Scene, and Shader abstractions now cooperate
    in order to abstract the basics from the main program. This will help
    port/create other backends for rendering in the future.

--- exp/cmd/helloworld/main.go
+++ exp/cmd/helloworld/main.go
@@ -4,15 +4,13 @@ import (
 	"os"
 	"runtime"
 
-	"github.com/go-gl/gl/v3.3-core/gl"
-	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/ronoaldo/openvoxel/log"
 	"github.com/ronoaldo/openvoxel/render"
 )
 
 var (
-	winWidth  int32 = 1024
-	winHeight int32 = 768
+	winWidth  int = 1024
+	winHeight int = 768
 
 	wireFrames bool
 )
@@ -22,26 +20,14 @@ func init() {
 }
 
 func main() {
-	if err := glfw.Init(); err != nil {
-		panic(err)
-	}
-	defer glfw.Terminate()
-
-	window, err := glfw.CreateWindow(int(winWidth), int(winHeight), "Hello World", nil, nil)
+	log.Infof("Initializing main window")
+	window, err := render.NewWindow(winWidth, winHeight, "Hello World")
 	if err != nil {
-		panic(err)
+		log.Warnf("Unable to open new window: %v", err)
 	}
-	window.MakeContextCurrent()
-	window.SetInputMode(glfw.StickyKeysMode, glfw.True)
-	window.SetFramebufferSizeCallback(func(w *glfw.Window, width, height int) {
-		gl.Viewport(0, 0, int32(width), int32(height))
-	})
+	defer window.Close()
+	log.Infof("Rendering Backend: %v", render.Version())
 
-	// Initialize
-	gl.Init()
-	log.Infof("OpenGL Version: %v", render.Version())
-
-	// Vertex Shader
 	shader := &render.Shader{}
 	shader.VertexShader("shaders/vertex.glsl").FragmentShader("shaders/fragment.glsl")
 	if err := shader.Link(); err != nil {
@@ -59,55 +45,13 @@ func main() {
 		0, 1, 3, // first triangle
 		1, 2, 3, // second triangle
 	}
-	log.Infof("Rendering Vertices: %#v", vertices)
+	log.Infof("Rendering Vertices: %#v (indices: %#v)", vertices, indices)
+	window.Scene().AddTriangles(vertices, indices)
 
-	var VAO uint32
-	gl.GenBuffers(1, &VAO)
-	gl.BindVertexArray(VAO)
-
-	var VBO uint32
-	gl.GenBuffers(1, &VBO)
-	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
-	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
-
-	var EBO uint32
-	gl.GenBuffers(1, &EBO)
-	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
-	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indices)*4, gl.Ptr(indices), gl.STATIC_DRAW)
-
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, nil)
-
-	gl.EnableVertexAttribArray(0)
-	gl.BindVertexArray(0)
-
+	// Main program loop
 	for !window.ShouldClose() {
-		processInput(window)
-
-		gl.ClearColor(0.2, 0.3, 0.3, 1.0)
-		gl.Clear(gl.COLOR_BUFFER_BIT)
-
-		shader.Use()
-		gl.BindVertexArray(VAO)
-		if wireFrames {
-			gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE)
-		} else {
-			gl.PolygonMode(gl.FRONT_AND_BACK, gl.FILL)
-		}
-		gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, nil)
-
+		window.Scene().Draw(shader)
 		window.SwapBuffers()
-		glfw.PollEvents()
-	}
-}
-
-func processInput(window *glfw.Window) {
-	if window.GetKey(glfw.KeyEscape) == glfw.Press {
-		log.Infof("ESC key pressed. Exiting...")
-		window.SetShouldClose(true)
-	}
-
-	if window.GetKey(glfw.KeyF10) == glfw.Press {
-		log.Infof("F10 key pressed. Flipping wireframe mode...")
-		wireFrames = !wireFrames
+		window.PollEvents()
 	}
 }
--- render/opengl.go
+++ render/opengl.go
@@ -3,10 +3,12 @@ package render
 
 import (
 	"errors"
+	"image/color"
 	"io/ioutil"
 	"strings"
 
 	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/ronoaldo/openvoxel/log"
 )
 
@@ -15,7 +17,103 @@ func Version() string {
 	return gl.GoStr(gl.GetString(gl.VERSION))
 }
 
-// ShaderFile is a helper struct that represents a shader file and it's type.
+// Window handles the basic GUI and Input event handling.
+//
+// Window must be created using NewWindow, which will load all the required
+// components of OpenGL and GLFW, as well as initializing a Scene object that
+// can be used to draw elements on. To add elements call the Scene() method.
+//
+// During the app main event loop, callers must make sure to call the
+// PollEvents() method to receive any window/input changes from the Operating
+// System.
+type Window struct {
+	window *glfw.Window
+	scene  *Scene
+
+	width  int
+	height int
+}
+
+// NewWindow initializes the program window and OpenGL backend.
+func NewWindow(width, height int, title string) (*Window, error) {
+	// Create our wrapper object and retain settings
+	w := &Window{}
+	w.width = width
+	w.height = height
+
+	// Initialize the GLFW window/context
+	if err := glfw.Init(); err != nil {
+		return nil, err
+	}
+
+	// Use glfw to create a new window
+	window, err := glfw.CreateWindow(int(width), int(height), title, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	w.window = window
+	w.window.MakeContextCurrent()
+	// Register GLFW callbacks
+	w.window.SetFramebufferSizeCallback(w.onWindowGeometryChanged)
+	w.window.SetKeyCallback(w.onKeyPressed)
+
+	// Initialize OpenGL
+	gl.Init()
+	w.scene = NewScene()
+
+	return w, nil
+}
+
+// ShouldClose returns true when the window must be closed. Before it should be
+// closed, it is safe to call any drawing operations. Once the window should be
+// closed is flipped to true, then callers must call Close() method to ensure
+// resources are properly freed.
+func (w *Window) ShouldClose() bool {
+	return w.window.ShouldClose()
+}
+
+// Close frees any used resources and close the underlying GLFW window.
+func (w *Window) Close() {
+	glfw.Terminate()
+}
+
+func (w *Window) onWindowGeometryChanged(wd *glfw.Window, width, height int) {
+	w.width = width
+	w.height = height
+	gl.Viewport(0, 0, int32(width), int32(height))
+}
+
+func (w *Window) onKeyPressed(wd *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	log.Infof("Key event received: key: %v, scancode: %v, action: %v, mods: %v", key, scancode, action, mods)
+
+	if key == glfw.KeyEscape && action == glfw.Press {
+		log.Infof("ESC key pressed. Exiting...")
+		w.window.SetShouldClose(true)
+	}
+
+	if key == glfw.KeyF10 && action == glfw.Press {
+		log.Infof("F10 key pressed. Flipping wireframe mode...")
+		w.scene.wireFrames = !w.scene.wireFrames
+	}
+}
+
+// PoolEvents listen to any window/input events to be passed to the input callbacks.
+func (w *Window) PollEvents() {
+	glfw.PollEvents()
+
+}
+
+// SwapBuffers will flip the drawing buffer to the visible buffer on the display.
+func (w *Window) SwapBuffers() {
+	w.window.SwapBuffers()
+}
+
+// Scene returns the Scene Graph used to draw on screen.
+func (w *Window) Scene() *Scene {
+	return w.scene
+}
+
+// shaderFile is a helper struct that represents a shader file and it's type.
 type shaderFile struct {
 	path       string
 	shaderType uint32
@@ -47,7 +145,7 @@ func (s *Shader) UniformInt(name string, value int32) error {
 	if s.program == nil {
 		return errors.New("shader: invalid state: uniform called before Link()")
 	}
-	gl.Uniform1i(gl.GetUniformLocation(*s.program, gl.Str(name)), value)
+	gl.Uniform1i(gl.GetUniformLocation(*s.program, gl.Str(name+"\x00")), value)
 	return nil
 }
 
@@ -57,7 +155,7 @@ func (s *Shader) UniformFloat(name string, value float32) error {
 	if s.program == nil {
 		return errors.New("shader: invalid state: uniform called before Link()")
 	}
-	gl.Uniform1f(gl.GetUniformLocation(*s.program, gl.Str(name)), value)
+	gl.Uniform1f(gl.GetUniformLocation(*s.program, gl.Str(name+"\x00")), value)
 	return nil
 }
 
@@ -150,4 +248,83 @@ func (s *Shader) linkProgram(shaders ...uint32) (uint32, error) {
 		gl.DeleteShader(shader)
 	}
 	return shaderProgram, nil
+}
+
+// Scene represents a graph of elements to be drawn on screen by the OpenGL
+// driver.
+type Scene struct {
+	vao *uint32
+	vbo *uint32
+	ebo *uint32
+
+	clearColor color.Color
+	wireFrames bool
+}
+
+// NewScene initializes an empty scene with the proper memory allocations.
+func NewScene() *Scene {
+	s := &Scene{}
+	s.allocateBuffers()
+	return s
+}
+
+func (s *Scene) allocateBuffers() {
+	if s.vao == nil {
+		s.vao = new(uint32)
+		s.vbo = new(uint32)
+		s.ebo = new(uint32)
+		gl.GenBuffers(1, s.vao)
+		gl.GenBuffers(1, s.vbo)
+		gl.GenBuffers(1, s.ebo)
+	}
+}
+
+// AddTriangles adds the provided vertices and indices to the current scene.
+func (s *Scene) AddTriangles(vertices []float32, indices []uint32) {
+	s.allocateBuffers()
+
+	gl.BindVertexArray(*s.vao)
+
+	gl.BindBuffer(gl.ARRAY_BUFFER, *s.vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, *s.ebo)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indices)*4, gl.Ptr(indices), gl.STATIC_DRAW)
+
+	// TODO(ronoaldo): 3 should be ... what???
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, nil)
+
+	gl.EnableVertexAttribArray(0)
+	gl.BindVertexArray(0)
+}
+
+// Draw calls the underlying driver to render the scene graph on the current
+// buffer.
+//
+// If the provided Shader program is not nil, it will be registered to be used
+// before rendering anything on screen.
+func (s *Scene) Draw(shader *Shader) {
+	s.allocateBuffers()
+
+	if s.clearColor == nil {
+		s.clearColor = color.Black
+	}
+	r, g, b, a := s.clearColor.RGBA()
+	gl.ClearColor(float32(r), float32(g), float32(b), float32(a))
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+
+	gl.BindVertexArray(*s.vao)
+	// TODO: use a default minimal shader program if no other shaders where specified
+	// since OpenGL requires a fragment and a vertex shader at a minimum.
+	if shader != nil {
+		shader.Use()
+	}
+
+	if s.wireFrames {
+		gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE)
+	} else {
+		gl.PolygonMode(gl.FRONT_AND_BACK, gl.FILL)
+	}
+
+	gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, nil)
 }
